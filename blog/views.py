d45c3606ab1da87291439bd79f4fcdcf655da7d5
62theories <s5935512005@phuket.psu.ac.th>
2019-02-27 00:23:18 +0000

    add homework

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+def showform(request):
+    return render(request,'blog/formtosearch.html')
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(title__contains= request.GET['value'])
+    return render(request, 'blog/post_list.html', {'posts': posts})
